5613f83e8369d5e836967a314938b45e8f3a44a0
Harsh Singh <141803481+harshsingh1605@users.noreply.github.com>
2024-12-17 23:53:43 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) => {
     const [ token, setToken ] = useState(localStorage.getItem('token'));
 
     const [credit, setCredit] = useState(0);
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "https://pixel-prompt-backend.onrender.com";
 
 
 
